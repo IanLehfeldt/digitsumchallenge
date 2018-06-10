194c9816f134ae8dd0b379b638cb3585ea22a6dc
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2018-06-09 20:11:33 -0500

    added in a palindrome checker for numbers that removes the last digit of a number, adds it to a variable and then for each loop around multiplies that varible by ten to add the next digit

--- src/com/ianl/Main.java
+++ src/com/ianl/Main.java
@@ -4,8 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println(sumDigits(146469182));
-        System.out.println(sumDigits(-100020005));
+//        System.out.println(sumDigits(146469182));
+//        System.out.println(sumDigits(-100020005));
+        System.out.println(isPalindrome(1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(-120021));
+        System.out.println(isPalindrome(12340));
     }
 
     public static int sumDigits(int number) {
@@ -18,5 +22,17 @@ public class Main {
             return sumOfAllDigits;
         }
         return -1;
+    }
+
+    public static boolean isPalindrome(int number) {
+        int originalNumber = number;
+        int reversedNumber = 0;
+        while(number!=0){
+            reversedNumber*=10;
+            reversedNumber+=(number%10);
+            number/=10;
+        }
+        //System.out.println("ReversedNumber: "+ reversedNumber);
+        return originalNumber == reversedNumber;
     }
 }
