8acf1c8bda81b76b044d09f43923e41ed475ee7c
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2018-06-09 20:48:47 -0500

    created a method that adds the last digit of a number to the first digit of a number by removing the last digit and continuing to remove digits till theres only one

--- src/com/ianl/Main.java
+++ src/com/ianl/Main.java
@@ -3,13 +3,17 @@ package com.ianl;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 //        System.out.println(sumDigits(146469182));
 //        System.out.println(sumDigits(-100020005));
-        System.out.println(isPalindrome(1221));
-        System.out.println(isPalindrome(707));
-        System.out.println(isPalindrome(-120021));
-        System.out.println(isPalindrome(12340));
+//        System.out.println(isPalindrome(1221));
+//        System.out.println(isPalindrome(707));
+//        System.out.println(isPalindrome(-120021));
+//        System.out.println(isPalindrome(12340));
+        System.out.println(firstLastDigitSum(2008));
+        System.out.println(firstLastDigitSum(156789));
+        System.out.println(firstLastDigitSum(2200));
+        System.out.println(firstLastDigitSum(9));
+
     }
 
     public static int sumDigits(int number) {
@@ -34,5 +38,19 @@ public class Main {
         }
         //System.out.println("ReversedNumber: "+ reversedNumber);
         return originalNumber == reversedNumber;
+    }
+
+    public static int firstLastDigitSum(int number) {
+        if(number >= 0) {
+            int firstNumber = number;
+            int lastNumber = number%10;
+
+            while(number >= 10) {
+                number/=10;
+                firstNumber = number;
+            }
+            return firstNumber + lastNumber;
+        }
+        return -1;
     }
 }
