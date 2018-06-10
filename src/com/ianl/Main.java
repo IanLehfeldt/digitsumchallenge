c15fdfbf8f6467172346f582b6745d2d9460b1bc
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2018-06-09 19:11:01 -0500

    challenge with loop statements

---
+++ src/com/ianl/Main.java
@@ -0,0 +1,12 @@
+package com.ianl;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static int sumDigits(int number) {
+        
+    }
+}
