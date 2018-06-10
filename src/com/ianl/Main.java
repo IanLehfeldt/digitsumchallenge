6b325ea015d57cc3a64d1f80b38570145cda6477
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2018-06-09 20:00:14 -0500

    created a method that takes in a number and does a while loop that adds the last digit to a variable and then removes it till theres no more numbers in the original

--- src/com/ianl/Main.java
+++ src/com/ianl/Main.java
@@ -4,9 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println(sumDigits(146469182));
+        System.out.println(sumDigits(-100020005));
     }
 
     public static int sumDigits(int number) {
-        
+        int sumOfAllDigits = 0;
+        if(number>=10) {
+            while(number>0){
+                sumOfAllDigits = sumOfAllDigits + (number % 10);
+                number/=10;
+            }
+            return sumOfAllDigits;
+        }
+        return -1;
     }
 }
